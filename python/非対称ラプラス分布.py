45dec0dc59cdc4c682eec662c51da2ceef9d4001
ngotik <nagaotaiki23@gmail.com>
2023-03-15 19:33:43 +0900

    分布を触っただけ

---
+++ python/非対称ラプラス分布.py
@@ -0,0 +1,26 @@
+import numpy as np
+import math
+import sympy as sym
+import matplotlib.pyplot as plt
+class ALD:
+    def random_density_function(x,mean,b):
+        return (1/(2*b))*math.exp(-(abs(x-mean)/b))
+    def cumulative_distribution_function(x,mean,b):
+        if x < mean:
+            return (1/2)*math.exp((x-mean)/b)
+        if x >= mean:
+            return 1-(1/2)*math.exp(-(x-mean)/b)
+        
+
+x=np.arange(-3,3,0.1)
+y=[]
+for i in range(len(x)):
+    y=np.append(y,ALD.random_density_function(x[i],0,1))
+plt.plot(x,y)
+plt.show()
+y=[]
+for i in range(len(x)):
+    y=np.append(y,ALD.cumulative_distribution_function(x[i],0,1))
+plt.plot(x,y)
+plt.show()
+
